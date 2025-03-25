a80d659a86ba1fa0fd572eb5f2194006818a0714
Collins Ezedike-egwom <ezedikecollins@gmail.com>
2025-03-25 06:22:12 +0100

    fix: wallet network appearing as number

--- src/commands/wallet.ts
+++ src/commands/wallet.ts
@@ -3,7 +3,11 @@ import { Message } from "node-telegram-bot-api";
 import bot from "../bot";
 
 import { CopperAPI } from "../utils/CopperAPI";
-import { getAccessToken } from "../utils/helpers";
+import {
+	getAccessToken,
+	getChainIDFromNetworkName,
+	getNetworkNameFromChainID,
+} from "../utils/helpers";
 
 export async function walletCommand(msg: Message) {
 	try {
@@ -39,9 +43,10 @@ export async function walletCommand(msg: Message) {
 				],
 			},
 		};
+		const network = getNetworkNameFromChainID(defaultWallet.network);
 		bot.sendMessage(
 			msg.chat.id,
-			`Your default wallet is ${defaultWallet.walletAddress} (Network: ${defaultWallet.network})\n\nYour balance is ${balance?.balance} ${balance?.symbol}\n\nWould you like to:`,
+			`Your default wallet is ${defaultWallet.walletAddress} (Network: ${network})\n\nYour balance is ${balance?.balance} ${balance?.symbol}\n\nWould you like to:`,
 			options
 		);
 	} catch (error: any) {
@@ -67,7 +72,8 @@ export async function getAllWallets(chat_id: number) {
 		const wallets = await new CopperAPI().getAllWallets(accessToken);
 		let message = "Here are your wallets:\n\n";
 		for (const wallet of wallets) {
-			message += `🔹${wallet.walletAddress} (Network: ${wallet.network})\n`;
+			const network = getNetworkNameFromChainID(wallet.network);
+			message += `🔹${wallet.walletAddress} (Network: ${network})\n`;
 		}
 		bot.sendMessage(chat_id, message);
 	} catch (error: any) {
@@ -90,8 +96,9 @@ export async function setDefaultWallet(chat_id: number) {
 		const wallets = await new CopperAPI().getAllWallets(accessToken);
 		const addresses: { text: string }[][] = [];
 		for (const wallet of wallets) {
+			const network = getNetworkNameFromChainID(wallet.network);
 			addresses.push([
-				{ text: `${wallet.walletAddress}\nNetwork: ${wallet.network}` },
+				{ text: `${wallet.walletAddress}\nNetwork: ${network}` },
 			]);
 		}
 		const options = {
@@ -105,9 +112,11 @@ export async function setDefaultWallet(chat_id: number) {
 			if (!msg.text) return;
 
 			const [walletAddress, network] = msg.text.split("\nNetwork: ");
+			const chainId = getChainIDFromNetworkName(network);
 			const wallet = wallets.find(
 				(w: any) =>
-					w.walletAddress === walletAddress && w.network === network
+					w.walletAddress === walletAddress &&
+					w.network === String(chainId)
 			);
 			if (!wallet) return bot.sendMessage(chat_id, "Wallet not found");
 
@@ -115,7 +124,9 @@ export async function setDefaultWallet(chat_id: number) {
 			bot.removeListener("message", messageHandler);
 			bot.sendMessage(
 				chat_id,
-				`New default wallet set.\n\nAddress: ${wallet.walletAddress}\nNetwork: ${wallet.network}`
+				`New default wallet set.\n\nAddress: ${
+					wallet.walletAddress
+				}\nNetwork: ${getNetworkNameFromChainID(wallet.network)}`
 			);
 		};
 		bot.on("message", messageHandler);
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,5 @@
+import { ethers } from "ethers";
+
 import { Session } from "./Session";
 
 export async function getAccessToken(chat_id: number) {
@@ -24,4 +26,15 @@ export async function getAccessToken(chat_id: number) {
 			accessToken,
 			message: { text: "", options: {} },
 		};
+}
+
+export function getNetworkNameFromChainID(network: string) {
+	const chainId = Number(network);
+	if (isNaN(chainId)) return network; // The network is acutally a string and not a number
+	return ethers.Network.from(chainId).name;
+}
+
+export function getChainIDFromNetworkName(networkName: string) {
+	const network = ethers.Network.from(networkName);
+	return network ? Number(network.chainId) : null;
 }
